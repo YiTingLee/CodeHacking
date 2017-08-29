9c3fb278ff2c398ccd36b19fe0faa22e316791f7
YiTingLee <ranye0719@gmail.com>
2017-08-29 16:39:57 +0800

    able to delete post

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -24,7 +24,7 @@
                         <td>{{$post->user->name}}</td>
                         <td>{{$post->category ? $post->category->name : 'Uncategorized'}}</td>
                         <td><img height="60" src="{{$post->photo ? $post->photo->file : 'http://placehold.it/400x400'}}" alt=""></td>
-                        <td>{{$post->title}}</td>
+                        <td><a href="{{route('admin.posts.edit', $post->id)}}">{{$post->title}}</a></td>
                         <td>{{$post->body}}</td>
                         <td>{{$post->created_at->diffForhumans()}}</td>
                         <td>{{$post->updated_at->diffForhumans()}}</td>
