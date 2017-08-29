2801fe5a5dd9e2c3a7e080688e232a09ddfebeac
YiTingLee <ranye0719@gmail.com>
2017-08-29 15:28:08 +0800

    my first commit

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Posts Page</h1>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>User</th>
+                <th>Category</th>
+                <th>Photo</th>
+                <th>Title</th>
+                <th>body</th>
+                <th>Created</th>
+                <th>Updated</th>
+            </tr>
+        </thead>
+        <tbody>
+            @if($posts)
+                @foreach($posts as $post)
+                    <tr>
+                        <td>{{$post->id}}</td>
+                        <td>{{$post->user->name}}</td>
+                        <td>{{$post->category ? $post->category->name : 'Uncategorized'}}</td>
+                        <td><img height="60" src="{{$post->photo ? $post->photo->file : 'http://placehold.it/400x400'}}" alt=""></td>
+                        <td>{{$post->title}}</td>
+                        <td>{{$post->body}}</td>
+                        <td>{{$post->created_at->diffForhumans()}}</td>
+                        <td>{{$post->updated_at->diffForhumans()}}</td>
+                    </tr>
+                @endforeach
+            @endif
+        </tbody>
+    </table>
+@stop
